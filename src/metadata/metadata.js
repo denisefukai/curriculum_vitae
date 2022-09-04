b5c4884d49c2906074dd2262218754340ad156cd
Denise Fukai <denise.fukai@gmail.com>
2022-09-03 23:26:48 -0300

    third commit

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -4,48 +4,72 @@ module.exports = {
   facts: {
     'Residence': `<a href="https://www.google.com/maps/place/Serra,+State+of+Esp%C3%ADrito+Santo/@-20.1616778,-40.2972365,12.04z/data=!4m5!3m4!1s0xb81fa3965b36af:0x521cd91111dd2e08!8m2!3d-20.1279854!4d-40.3081077">
                   <i class="fa fa-home fact-icon"></i>Serra/ES - BR</a>`,
-    'LinkedIn': '<a href="http://www.linkedin.com/in/denisefukai"><i class="fab fa-linkedin fast-icon </i>denisefukai</a>',
-    'GitHub': '<a href="http://github.com/denisefukai"><i class="fab fa-github fact-icon></i> denisefukai</a>',
+    'LinkedIn': '<a href="http://www.linkedin.com/in/denisefukai"> <i class="fab fa-linkedin fact-icon"></i>denisefukai</a>',
+    'GitHub': '<a href="http://github.com/denisefukai"><i class="fab fa-github fact-icon"></i> denisefukai</a>',
     'Email': '<a href="mailto:denise.fukai@gmail.com"><i class="fa fa-envelope fact-icon"></i>denise.fukai@gmail.com</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['Portuguese', 100],
+    ['English', 80],
+    ['Spanish', 40],
+    ['Python', 70],
+    ['SQL', 100],
+    ['Posgres', 75],
+    ['CSS', 100],
+    ['HMTL', 100],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      title: "Young Talents in Science Program",
+      period: '2012 - 2013',
+      skills: ['matlab', 'adcirc', 'coastal modelling'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+    UFES - Coordination for the Improvement of Higher Education (Capes) - Brazil 
+    Project: Numerical Modelling Development using ADCIRC
+    Supervisor: Julio Tomaz Aquije Chacaltana
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
+      title: 'Brazilian Scientific Mobility Program: Science without Borders',
+      period: 'Feb/2014 - Jun/2015',
+      skills: ['english'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+    National Council for Research and Development (CNPq) - Brazil
+    Exchange scholarship: 6-months language course and a year of a graduate course at UNSW`
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'ARCCSS Summer Research Scholarship',
+      period: 'Dec/2014 - Feb/2015',
+      skills: ['ENSO', 'mode variability'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+    Project: “Ocean-atmosphere interaction analysis through the first version of 
+    recharge oscillator model”
+    ARCCSS / IMAS - Australia
+    Supervisor: Eric Oliver, Neil Holbrook, Catia Domingues, James Risbey`
+    },
+
+    {
+      title: 'Oceanografia para Todos (Oceanography for Everyone)',
+      period: '2016 - 2017',
+      skills: ['community outreach', 'extension project'],
+      contents: `
+    Co-funder of an extension group to disseminate oceanography to the general public. 
+    The focus was on the translation of science and research projects through public 
+    communication. The project has now been implemented as a long term Extension 
+    Programme at University (UFES) by current members.`
+    },
+
+    {
+      title: 'CAPES Beach Morphodynamic Response of the Southeast of Brazil to the Sea Level Rise and Extremes Meteo-oceanographic Events until 2100',
+      period: '2017 - present',
+      skills: ['data science', 'wave analysis', 'climate projection', 'coastal'],
+      contents: `
+    Research Collaborator (Data Analyst Support)
+    Project: This 4 years project aims to understand the short and long term local impacts 
+    and projections for the southeast Brazilian coast due to climate change. The analysis 
+    focus on extreme wave events, sea level and hydrodynamical changes and combined coastal 
+    inundation processes.
+    CAPES project registration no: 88887.145855/2017.00`
     }
   ],
   experience: [
